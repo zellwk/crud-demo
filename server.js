4828ea5c7827c8ffa5a8a40f66890f4bbb263568
Zell Liew <zellwk@gmail.com>
2020-04-08 18:53:28 +0800

    Change port

--- server.js
+++ server.js
@@ -75,8 +75,9 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     // ========================
     // Listen
     // ========================
-    app.listen(3000, function () {
-      console.log('listening on 3000')
+    const port = process.env.PORT || 3000
+    app.listen(port, function () {
+      console.log(`listening on ${port}`)
     })
   })
   .catch(console.error)
